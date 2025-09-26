75443aa01480a550d5758383df91820c2b475e63
Vickins1 <vickinstechnologies@gmail.com>
2025-09-26 20:27:30 +0300

    updates

--- next.config.ts
+++ next.config.ts
@@ -1,7 +1,11 @@
 import type { NextConfig } from "next";
+import path from "path";
 
 const nextConfig: NextConfig = {
-  outputFileTracingRoot: "C:\\Users\\ABCD",
+  // Set outputFileTracingRoot dynamically based on the environment
+  outputFileTracingRoot: process.env.VERCEL
+    ? "/vercel/path0"
+    : path.resolve(__dirname),
 };
 
 export default nextConfig;
